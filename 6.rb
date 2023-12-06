4d8983477d577d9571fe4e77235f8bfdb3bdd47c
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-12-06 12:05:47 +0100

    Refactor 6 to go faster

--- 6.rb
+++ 6.rb
@@ -1,15 +1,19 @@
+def ways(time, distance)
+  time+1-(1..).find { |push| (time-push)*push > distance }*2
+end
+
 def solve(input)
   p input
     .split("\n")
     .map { |line| line.split[1..].map(&:to_i) }
     .inject { |times, distances| times.zip(distances) }
-    .map { |time, distance| (0..time).count { |push| (time-push)*push > distance } }
+    .map { |time, distance| ways time, distance }
     .inject(:*)
   
   p input
     .split("\n")
     .map { |line| line.split(':')[1].delete(' ').to_i }
-    .inject { |time, distance| (0..time).count { |push| (time-push)*push > distance } }
+    .inject { |time, distance| ways time, distance }
 end
 
 TEST_INPUT = <<~EOF
@@ -22,5 +26,5 @@ Time:        63     78     94     68
 Distance:   411   1274   2047   1035
 EOF
 
-solve(TEST_INPUT)
-solve(INPUT)
+solve TEST_INPUT
+solve INPUT
